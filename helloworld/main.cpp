68be0aaaedef767d72b91fa24d0c24f3b2055f8b
Tomiwa Ijaware <Tomiwa_Ijaware@mckinsey.com>
2017-01-03 23:54:39 +0100

    Initial Commit

---
+++ helloworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Tomiwa Ijaware on 03/01/2017.
+//  Copyright © 2017 Tomiwa Ijaware. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
